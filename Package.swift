3636edc1bb8884a5e202c436e00a03b2b8d1ffac
dimakovmir <sprusenok@admixer.ua>
2024-10-08 14:36:22 +0300

    first commit

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "AdmixerSDK",
+    platforms: [.iOS(.v12)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
@@ -12,10 +13,12 @@ let package = Package(
             targets: ["AdmixerSDK"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "AdmixerSDK"),
+            // Targets are the basic building blocks of a package, defining a module or a test suite.
+            // Targets can depend on other targets in this package and products from dependencies.
+            .binaryTarget(name: "AdmixerSDK",
+                          url: "https://admixercloudcreatives.blob.core.windows.net/public/ios-sdk/AdmixerSDK_0.8.6.xcframework.zip",
+                          checksum: "ed659bacc45d716ea9df78eb25c2555d50e2e63f43baf8bc25f526925ec9f7fe")
 
-    ]
+        ]
 )
+
